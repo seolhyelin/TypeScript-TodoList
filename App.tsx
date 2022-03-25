8903308317ee4c27c7def67778ccc138bf7a62fa
설혜린 <tosiltosilkk@gmail.com>
2022-03-25 16:06:13 +0900

    Add: list 불러오기

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TodoListItem } from "./TodoListItem";
+
+const todos: Array<Todo> = [
+  { text: "Walk the dog", complete: true },
+  { text: "Write app", complete: false },
+];
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <TodoListItem todo={todos[0]} />;
+      <TodoListItem todo={todos[1]} />;
+    </React.Fragment>
+  );
+};
+
+export default App;
